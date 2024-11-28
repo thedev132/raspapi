bd16dcc27fb88cd2afeb7255ea4d3abb2e0e394f
ByteAtATime <byteatatime@proton.me>
2024-11-28 10:13:29 -0800

    fix: use server instead of hybrid rendering

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,6 +8,6 @@ import vercel from '@astrojs/vercel/serverless';
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  output: 'hybrid',
+  output: 'server',
   adapter: vercel()
 });
