a5b5246fb6a8ef3e78791c825dcca8a2836ba805
ByteAtATime <byteatatime@proton.me>
2024-11-28 10:03:05 -0800

    feat: add top 3 prize

---
+++ src/pages/prizes.ts
@@ -0,0 +1,12 @@
+import type { APIRoute } from "astro";
+
+const res = `
+{
+    top_3: "Raspberry Pi Sensor HAT",
+    about: "The Sensor HAT is a great addition to your Raspberry Pi, allowing you to measure temperature, humidity, and more!",
+}
+`
+
+export const GET: APIRoute = () => {
+    return new Response(res);
+}
